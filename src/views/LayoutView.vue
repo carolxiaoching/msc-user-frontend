8f7ebb4c0dce9504ad77c7c85d4fd2817245acbb
xiao ching <carol.xiao.ching@gmail.com>
2025-03-24 12:32:09 +0800

    first commit

---
+++ src/views/LayoutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <NavbarComponent />
+  <main class="main" :class="{ 'container-lg': $route.name !== 'home' }">
+    <RouterView />
+  </main>
+  <FooterComponent />
+</template>
+
+<script setup>
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+</script>
